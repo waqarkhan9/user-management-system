7886aad011382a2b066ce8d6c1d2cf4ba2e4060e
waqar khan <waqarkhan9@yahoo.com>
2026-01-02 19:20:07 -0800

    index js up and running

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,46 @@
-//hello world
+import dotenv from "dotenv";
+import mysql from "mysql";
+import cors from "cors";
+import express, { json } from "express";
+import bodyParser from "body-parser";
+
+
+
+dotenv.config();
+const app = express();
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(cors());
+
+app.get("/user", (req, res) => {
+  const q = "SELECT * FROM user";
+  conn.query(q,(err, data)=>{
+    if (err) return err;
+    res.send(data);
+  });
+});
+
+app.post("/user", (req, res) => {
+  const q = "INSERT INTO user (`first_name`, `last_name`) VALUES (?)";
+  const values = [req.body.first_name, req.body.last_name];
+  conn.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("user has been created successfully");
+  });
+});
+
+
+
+
+
+app.listen(3000,()=>{
+  console.log("connect to backend and server is running");
+});
